9522779cdd619f10a08439ad73271aa82a4b4e16
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-02-11 01:25:08 +0600

    withdraw from main balance feature added

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,4 @@
+// Deposite features
 document.getElementById('primary-deposite').addEventListener('click',function() {
     let inputFieldText = document.getElementById('deposite-amount').value;
     let inputField = parseFloat(inputFieldText);
@@ -7,9 +8,25 @@ document.getElementById('primary-deposite').addEventListener('click',function() 
     let DepositeFloat = parseFloat(DepositeText);
     Deposite.innerText = DepositeFloat + inputField;
     document.getElementById('deposite-amount').value = '';
-    // total balance
+    // total balance after deposite
     let totalBalance = document.getElementById('total-bal');
     let balanceText = totalBalance.innerText;
     let Balancefloat = parseFloat(balanceText);
     totalBalance.innerText = Balancefloat + inputField;
 });
+// withdraw featers
+document.getElementById('withdraw-amount').addEventListener('click',function () {
+    let inputFieldText = document.getElementById('input-field').value;
+    let inputField = parseFloat(inputFieldText);
+    // total withdraw
+    let totalWithdraw = document.getElementById('total-withdraw')
+    let totalWithdrawText = totalWithdraw.innerText;
+    let totalWithdrawFloat = parseFloat(totalWithdrawText);
+    totalWithdraw.innerText = totalWithdrawFloat + inputField;
+    document.getElementById('input-field').value = '';
+    // total balance after withdraw
+    let totalBalance = document.getElementById('total-bal');
+    let balanceText = totalBalance.innerText;
+    let Balancefloat = parseFloat(balanceText);
+    totalBalance.innerText = Balancefloat - inputField;
+})
