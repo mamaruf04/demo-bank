acb4911852f68fa46221755df4c9f384c2190f7b
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-02-11 00:49:20 +0600

    filename issu solved

--- js/login.js
+++ js/login.js
@@ -3,6 +3,6 @@ document.getElementById('login-button').addEventListener('click',function() {
     const emailField = document.getElementById('input-email').value;
     const passwordField = document.getElementById('input-password').value;
     if(emailField == "maruf@admin.com" && passwordField == "1234"){
-        window.location.href = 'index.html';
+        window.location.href = 'home.html';
     }
 })
