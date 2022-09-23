67510e6c4f49bb949c16e04fd621a3e4389cf7f4
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-09-23 16:20:44 +0600

    error hendeling added

--- js/bank.js
+++ js/bank.js
@@ -3,16 +3,27 @@ document.getElementById('primary-deposite').addEventListener('click',function() 
     let inputFieldText = document.getElementById('deposite-amount').value;
     let inputField = parseFloat(inputFieldText);
     // total deposite
-    let Deposite = document.getElementById('diposite');
-    let DepositeText = Deposite.innerText;
+    const Deposite = document.getElementById('diposite');
+    const DepositeText = Deposite.innerText;
     let DepositeFloat = parseFloat(DepositeText);
-    Deposite.innerText = DepositeFloat + inputField;
     document.getElementById('deposite-amount').value = '';
     // total balance after deposite
-    let totalBalance = document.getElementById('total-bal');
-    let balanceText = totalBalance.innerText;
+    const totalBalance = document.getElementById('total-bal');
+    const balanceText = totalBalance.innerText;
     let Balancefloat = parseFloat(balanceText);
-    totalBalance.innerText = Balancefloat + inputField;
+
+    if(inputField > 0){
+        Deposite.innerText = DepositeFloat + inputField;
+        totalBalance.innerText = Balancefloat + inputField;
+    } 
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'You can not input minus figure or empty field!',
+          })
+    }
+    
 });
 // withdraw featers
 document.getElementById('withdraw-amount').addEventListener('click',function () {
@@ -22,11 +33,22 @@ document.getElementById('withdraw-amount').addEventListener('click',function () 
     let totalWithdraw = document.getElementById('total-withdraw')
     let totalWithdrawText = totalWithdraw.innerText;
     let totalWithdrawFloat = parseFloat(totalWithdrawText);
-    totalWithdraw.innerText = totalWithdrawFloat + inputField;
+    
     document.getElementById('input-field').value = '';
     // total balance after withdraw
     let totalBalance = document.getElementById('total-bal');
     let balanceText = totalBalance.innerText;
     let Balancefloat = parseFloat(balanceText);
+
+    if(totalBalance.innerText >= inputField  && inputField > 0){
+    totalWithdraw.innerText = totalWithdrawFloat + inputField;
     totalBalance.innerText = Balancefloat - inputField;
+    }
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'You can not withdraw more then your Balance or empty!',
+          })
+    }
 })
